3cb364f22d30ce54b2e6724e7f6473964c26ddc5
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-10-01 22:47:35 +0100

    Added calendly scheduler

---
+++ src/Scheduler.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <vue-calendly id="scheduler" url="https://calendly.com/leo-pfeiffer?hide_gdpr_banner=1" :height="600">
+    </vue-calendly>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Scheduler',
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #42b983;
+  width: 100%;
+  height: 100%;
+}
+
+#scheduler {
+  margin: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 620px;
+  max-width: 90vw;
+  -moz-transform: translateX(-50%) translateY(-50%);
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+  border-radius: 10px;
+  box-shadow: 0 0px 10px rgba(0, 0, 0, 0.4);
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Scheduler from './Scheduler.vue'
+import VueCalendly from 'vue-calendly';
+
+Vue.use(VueCalendly);
+
+
+const NotFound = { template: '<p>Page not found</p>' }
+const About = { template: '<p>about page</p>' }
+
+const routes = {
+  '/': App,
+  '/scheduler': Scheduler,
+  '/about': About
+}
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
