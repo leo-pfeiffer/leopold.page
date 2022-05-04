1d7250e05c5fda9896048333c390a4b8b9125695
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-05-04 14:32:24 +0100

    feat: fix dark mode butotn

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="app" class="h-screen flex items-start justify-center">
       <div class="container">
-        <header class="sm:sticky ml-8 top-0 w-full flex text-xs text-black dark:text-white float-right">
-          <span class="inline-block align-middle bg-white dark:bg-monokai-blue flex py-8">
-            <button id="darkmode-button" class="text-xs p-1 rounded-md" v-on:click="toggleButton">
-              <svg v-if="darkMode" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" stroke="white" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"/>
-              </svg>
-              <svg v-if="!darkMode" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="black">
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"/>
-              </svg>
-            </button>
-          </span>
-        </header>
-        <div class="text-monokai-red text-5xl text-center mt-4">
-          {{ name }}
+
+        <nav class="py-4">
+          <div class="container px-4 flex">
+            <span class="bg-white dark:bg-monokai-blue align-center ml-auto">
+              <button id="darkmode-button" class="text-xs p-1 rounded-md bg-monokai-gray" v-on:click="toggleButton">
+                <svg v-if="darkMode" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" stroke="white" fill="white" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"/>
+                </svg>
+                <svg v-if="!darkMode" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="white">
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"/>
+                </svg>
+              </button>
+            </span>
+          </div>
+        </nav>
+
+        <div class="text-monokai-red text-center text-5xl mt-10">
+          <span>{{ name }} </span>
         </div>
+
         <div>
           <div class="w-5/6 max-w-4xl ml-auto mr-auto mt-16 mb-8 text-center justify-center">
             <div class="grid place-items-center">
